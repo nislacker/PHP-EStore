ce9998131007664addf4ada54c30e2828ccbe321
Nislacker <nislacker@gmail.com>
2019-03-12 14:47:29 +0200

    1st commit

---
+++ template/js/main.js
@@ -0,0 +1,31 @@
+/*price range*/
+
+$('#sl2').slider();
+
+var RGBChange = function () {
+    $('#RGB').css('background', 'rgb(' + r.getValue() + ',' + g.getValue() + ',' + b.getValue() + ')')
+};
+
+/*scroll to top*/
+
+$(document).ready(function () {
+
+    $('#myCarousel').carousel({
+        interval: 10000
+    })
+
+    $('.carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
